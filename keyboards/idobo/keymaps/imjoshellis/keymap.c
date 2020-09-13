d52c1f0705607b4d99a694254268273f4ff49985
Josh Ellis <josh@imjoshellis.com>
2020-09-13 13:09:02 -0500

    Switch zoom out/reset to match vol controls

--- keyboards/idobo/keymaps/imjoshellis/keymap.c
+++ keyboards/idobo/keymaps/imjoshellis/keymap.c
@@ -151,7 +151,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_5x15(
     //  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MUTE, KC_VOLD, KC_VOLU, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, 
-        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    Z_OUT,   Z_RESET, Z_IN,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS, 
+        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    Z_RESET, Z_OUT,   Z_IN,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS, 
         LC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    FLLSCRN, OSL(3),  DIS_NCH, KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, 
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_MPRV, KC_MPLY, KC_MNXT, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT, 
         MO(3),   KC_HYPR, KC_LCTL, KC_LALT, KC_LGUI, MO(2),   KC_MEH,  TG(5),   SG_BSPC, KC_SPC,  MO(1),   KC_HYPR, KC_DOWN, KC_UP,   MO(12)
